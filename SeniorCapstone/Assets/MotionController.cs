eeb0e0ef51709f6bbd1b6c4e42f56085cb1f1307
Luciano Fasani <lucianofasani@gmail.com>
2017-11-14 23:31:47 -0600

    Modified redundancies in controller tracking
    I optimized the code for checking controller presses more and have just
    began object interaction. AKA controller interaction with colliders. Not
    complete just yet.

--- SeniorCapstone/Assets/MotionController.cs
+++ SeniorCapstone/Assets/MotionController.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class MotionController : MonoBehaviour {
 
+    
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
-    public bool gripButtonDown = false;
-    public bool gripButtonUp = false;
-    public bool gripButtonPressed = false;
-
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
-    public bool triggerButtonDown = false;
-    public bool triggerButtonUp = false;
-    public bool triggerButtonPressed = false;
 
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
     private SteamVR_TrackedObject trackedObj;
+
+    private GameObject pickup;
 
 
     // Use this for initialization
@@ -32,13 +28,36 @@ public class MotionController : MonoBehaviour {
             return;
         }
 
-        gripButtonDown = controller.GetPressDown(gripButton);
-        gripButtonUp = controller.GetPressUp(gripButton);
-        gripButtonPressed = controller.GetPress(gripButton);
+        if(controller.GetPressDown(gripButton) && pickup != null)
+        {
+            pickup.transform.parent = this.transform;
+            pickup.GetComponent<Rigidbody>().useGravity = false; //This allows the object to be freely picked up and moved around.
+        }
 
-        triggerButtonDown = controller.GetPressDown(triggerButton);
-        triggerButtonUp = controller.GetPressUp(triggerButton);
-        triggerButtonPressed = controller.GetPress(triggerButton);
-
+        if(controller.GetPressUp(gripButton) && pickup != null)
+        {
+            pickup.transform.parent = null;
+            pickup.GetComponent<Rigidbody>().useGravity = true; //Make the held object be affected by gravity again.
+        }
+    
     }
+
+    /*
+     * Tracks when the controller goes into a space that contains a collider
+     */
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("Trigger entered");
+        pickup = collider.gameObject;
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        Debug.Log("Trigger exit");
+        pickup = null;
+    }
+
+
+    
+
 }
