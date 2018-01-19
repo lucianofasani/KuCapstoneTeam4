db783d5c5d4eb922f0b8ff0d75db4666480f2d54
Luciano Fasani <lucianofasani@gmail.com>
2018-01-18 23:26:45 -0600

    Added a laser to the end of the controller. Mainly learning how to track aiming and hitting objects at a distance.

---
+++ SeniorCapstone/Assets/Scripts/LaserPointer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPointer : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    public GameObject laserPrefab;
+    public GameObject laser;
+    private Transform laserTransform;
+    private Vector3 hitPoint;
+
+    private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
+
+    private void ShowLaser(RaycastHit hit)
+    {
+        laser.SetActive(true);
+        laserTransform.position = Vector3.Lerp(trackedObj.transform.position, hitPoint, .5f);
+        laserTransform.LookAt(hitPoint);
+        laserTransform.localScale = new Vector3(laserTransform.localScale.x, laserTransform.localScale.y, hit.distance);
+    }
+
+    void Start()
+    {
+        laser = Instantiate(laserPrefab);
+        laserTransform = laser.transform;
+    }
+
+    // Use this for initialization
+    void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(trackedObj.transform.position, transform.forward, out hit, 100))
+            {
+                hitPoint = hit.point;
+                ShowLaser(hit);
+            }
+        }
+        else
+        {
+            laser.SetActive(false);
+        }
+        
+	}
+}
